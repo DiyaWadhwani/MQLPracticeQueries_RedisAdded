22ec62d77e767758410152e37170ee8cf3fb53d3
Diya Wadhwani <wadhwani2399@gmail.com>
2024-11-03 21:13:37 -0800

    Modified Query1

--- Query1.js
+++ Query1.js
@@ -6,8 +6,19 @@ async function main() {
   try {
     const database = client.db("ieeevisTweets");
     const tweets = database.collection("tweet");
-    const query = { retweeted_status: { $exists: false } };
-    const result = await tweets.find(query).toArray();
+    const query = {
+      retweeted_status: { $exists: false },
+      in_reply_to_status_id: null,
+    };
+    const result = await tweets
+      .find(query)
+      .project({
+        _id: 0,
+        text: 1,
+        in_reply_to_status_id: 1,
+        retweeted_status: 1,
+      })
+      .toArray();
     console.log(
       "Number of tweets that are not rewteets or replies:",
       result.length
