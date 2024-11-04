3f700f35ad97a2d65a5e290f77907376f40dedb6
Diya Wadhwani <wadhwani2399@gmail.com>
2024-11-03 20:06:55 -0800

    Modified Query1.js

--- Query1.js
+++ Query1.js
@@ -1,18 +1,20 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 async function main() {
-  await client.connect();
-  const database = client.db("ieeevisTweets");
-  const collection = database.collection("tweet");
-  const query = { retweeted_status: { $exists: false } };
-  const result = await collection.find(query).toArray();
-  console.log(
-    "Number of tweets that are not rewteets or replies:",
-    result.length
-  );
-  await client.close();
+  try {
+    const database = client.db("ieeevisTweets");
+    const tweets = database.collection("tweet");
+    const query = { retweeted_status: { $exists: false } };
+    const result = await tweets.find(query).toArray();
+    console.log(
+      "Number of tweets that are not rewteets or replies:",
+      result.length
+    );
+  } finally {
+    await client.close();
+  }
 }
 
 main().catch(console.error);
